6ab530770fc2b31e76f4c9af702077f460ef2ba8
nosoop <nosoop@users.noreply.github.com>
2019-07-23 09:01:49 -0700

    Remove unused static calls

--- utils/RCBot2_meta/bot_plugin_meta.h
+++ utils/RCBot2_meta/bot_plugin_meta.h
@@ -141,9 +141,6 @@ public:
 	const char *GetDate();
 	const char *GetLogTag();
 
-	static bool UTIL_TF2EquipHat(edict_t *pEdict, CTF2Loadout *pHat, void *vTable, void *vTableAttributes);
-	static CTF2Loadout *UTIL_TF2EquipRandomHat(edict_t *pEdict, void *vTable, void *vTableAttributes);
-
 private:
 	int m_iClientCommandIndex;
 
