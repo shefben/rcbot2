51ad988b5537827bd09a17981439b01beaa9a977
nosoop <nosoop@users.noreply.github.com>
2020-05-12 21:11:38 -0700

    Use SDK copy of IServerTools

--- utils/RCBot2_meta/bot.h
+++ utils/RCBot2_meta/bot.h
@@ -46,6 +46,7 @@
 
 //#include "cbase.h"
 //#include "baseentity.h"
+#include "toolframework/itoolentity.h"
 #include "filesystem.h"
 #include "interface.h"
 #include "engine/iserverplugin.h"
@@ -1101,66 +1102,6 @@ class IClientEntity;
 class CBaseAnimating;
 class ITempEntsSystem;
 class CTakeDamageInfo;
-//-----------------------------------------------------------------------------
-// Purpose: Interface from engine to tools for manipulating entities
-//-----------------------------------------------------------------------------
-class IServerTools : public IBaseInterface
-{
-public:
-	virtual IServerEntity *GetIServerEntity( IClientEntity *pClientEntity ) = 0;
-	virtual bool SnapPlayerToPosition( const Vector &org, const QAngle &ang, IClientEntity *pClientPlayer = NULL ) = 0;
-	virtual bool GetPlayerPosition( Vector &org, QAngle &ang, IClientEntity *pClientPlayer = NULL ) = 0;
-	virtual bool SetPlayerFOV( int fov, IClientEntity *pClientPlayer = NULL ) = 0;
-	virtual int GetPlayerFOV( IClientEntity *pClientPlayer = NULL ) = 0;
-	virtual bool IsInNoClipMode( IClientEntity *pClientPlayer = NULL ) = 0;
-
-	// entity searching
-	virtual CBaseEntity *FirstEntity( void ) = 0;
-	virtual CBaseEntity *NextEntity( CBaseEntity *pEntity ) = 0;
-	virtual CBaseEntity *FindEntityByHammerID( int iHammerID ) = 0;
-
-	// entity query
-	virtual bool GetKeyValue( CBaseEntity *pEntity, const char *szField, char *szValue, int iMaxLen ) = 0;
-	virtual bool SetKeyValue( CBaseEntity *pEntity, const char *szField, const char *szValue ) = 0;
-	virtual bool SetKeyValue( CBaseEntity *pEntity, const char *szField, float flValue ) = 0;
-	virtual bool SetKeyValue( CBaseEntity *pEntity, const char *szField, const Vector &vecValue ) = 0;
-
-	// entity spawning
-	virtual CBaseEntity *CreateEntityByName( const char *szClassName ) = 0;
-	virtual void DispatchSpawn( CBaseEntity *pEntity ) = 0;
-
-	// This reloads a portion or all of a particle definition file.
-	// It's up to the server to decide if it cares about this file
-	// Use a UtlBuffer to crack the data
-	virtual void ReloadParticleDefintions( const char *pFileName, const void *pBufData, int nLen ) = 0;
-
-	virtual void AddOriginToPVS( const Vector &org ) = 0;
-	virtual void MoveEngineViewTo( const Vector &vPos, const QAngle &vAngles ) = 0;
-
-	virtual bool DestroyEntityByHammerId( int iHammerID ) = 0;
-	virtual CBaseEntity *GetBaseEntityByEntIndex( int iEntIndex ) = 0;
-	virtual void RemoveEntity( CBaseEntity *pEntity ) = 0;
-	virtual void RemoveEntityImmediate( CBaseEntity *pEntity ) = 0;
-	virtual IEntityFactoryDictionary *GetEntityFactoryDictionary( void ) = 0;
-
-	virtual void SetMoveType( CBaseEntity *pEntity, int val ) = 0;
-	virtual void SetMoveType( CBaseEntity *pEntity, int val, int moveCollide ) = 0;
-	virtual void ResetSequence( CBaseAnimating *pEntity, int nSequence ) = 0;
-	virtual void ResetSequenceInfo( CBaseAnimating *pEntity ) = 0;
-
-	virtual void ClearMultiDamage( void ) = 0;
-	virtual void ApplyMultiDamage( void ) = 0;
-	virtual void AddMultiDamage( const CTakeDamageInfo &pTakeDamageInfo, CBaseEntity *pEntity ) = 0;
-	virtual void RadiusDamage( const CTakeDamageInfo &info, const Vector &vecSrc, float flRadius, int iClassIgnore, CBaseEntity *pEntityIgnore ) = 0;
-
-	virtual ITempEntsSystem *GetTempEntsSystem( void ) = 0;
-};
-
-typedef IServerTools IServerTools001;
-
-#define VSERVERTOOLS_INTERFACE_VERSION_1	"VSERVERTOOLS001"
-#define VSERVERTOOLS_INTERFACE_VERSION		"VSERVERTOOLS002"
-#define VSERVERTOOLS_INTERFACE_VERSION_INT	2
 
 void DrawLine ( const Vector &origin, const Vector &target, int r, int g, int b, bool noDepthTest, float duration );
 
